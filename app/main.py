ec5af46a814315945fdb7f7269f367879bd8d3b8
Vadym Hutei <hutei@live.com>
2019-09-04 00:36:21 +0300

    rendering templates

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Nadia'
+    return render_template('main.html')
