5a2c6d098acc821d8b39f2750889183f7df46781
Vadym Hutei <hutei@live.com>
2019-10-05 15:36:01 +0300

    [GALLERY RESIZE]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import os.path as p
+from os import makedirs
+from PIL import Image
+from flask import Flask, render_template, abort, request, send_from_directory
 
 app = Flask(__name__)
 
@@ -9,6 +12,46 @@ def hello():
 @app.route('/galleries/')
 def galleries():
     return render_template('galleries.html')
+
+@app.route('/static/galleries/<path:path>')
+def images(path):
+    path = p.join('static', 'galleries', path)
+    image = p.basename(path)
+    directory = p.dirname(path)
+    image_l = image.split('.')
+    image_ext = image_l[-1]
+    image_name = '.'.join(image_l[:-1])
+    if not p.exists(path): abort(400)
+    width = request.args.get('w', None)
+    height = request.args.get('h', None)
+    if width is None and height is None:
+        return send_from_directory(directory, image)
+    rsz_directory = p.join(directory, 'rsz')
+    if not p.exists(rsz_directory):
+        makedirs(rsz_directory)
+    rsz_image_name = image_name
+    if width is not None and width.isdigit():
+        rsz_image_name += '_w' + width
+    if height is not None and height.isdigit():
+        rsz_image_name += '_h' + height
+    rsz_image = rsz_image_name + '.' + image_ext
+    rsz_path = p.join(rsz_directory, rsz_image)
+    if p.exists(rsz_path):
+        return send_from_directory(rsz_directory, rsz_image)
+    orig_img = Image.open(path)
+    w, h = orig_img.size
+    if width and height:
+        max_size = (int(width), int(height))
+    elif width:
+        max_size = (int(width), h)
+    elif height:
+        max_size = (w, int(height))
+    else:
+        abort(400)
+    orig_img.thumbnail(max_size, Image.ANTIALIAS)
+    orig_img.save(rsz_path)
+    if p.exists(rsz_path):
+        return send_from_directory(rsz_directory, rsz_image)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
