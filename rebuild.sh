f9bfcc03a87c9a782632f62d7d268724647f3929
Vadym Hutei <hutei@live.com>
2019-10-05 19:33:39 +0300

    [REBUILD]

--- rebuild.sh
+++ rebuild.sh
@@ -5,4 +5,4 @@ docker rm nadia
 docker rmi nadia
 git pull origin master
 docker build -t nadia .
-docker run -d --restart always --name nadia -p 80:80 -v /home/nadia/galleries:/app/static/galleries nadia
+docker run -d --restart always --name nadia -p 80:80 -v /home/galleries:/app/static/galleries nadia
