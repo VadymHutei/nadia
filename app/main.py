d788880063ada3d3a4361a314d619101c79e9666
Vadym Hutei <hutei@live.com>
2019-10-15 22:25:38 +0300

    [GALLERIES]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from os import makedirs, listdir, path as p
 from PIL import Image
 from flask import Flask, render_template, abort, request, send_from_directory
+from lib import getUri
 
 app = Flask(__name__)
 
@@ -14,34 +15,37 @@ def galleries():
     galleries_names = listdir('galleries')
     for gallery_name in galleries_names:
         gallery_path = p.join('galleries', gallery_name)
+        gallery_uri = getUri(gallery_name)
         photos = listdir(gallery_path)
         if not photos: continue
         if 'thumbnail.jpg' not in photos: continue
-        thumbnail_path = p.join('img', gallery_path, 'thumbnail.jpg?h=280')
         galleries.append({
             'name': gallery_name,
-            'link': '/' + gallery_path,
-            'thumbnail': '/' + thumbnail_path
+            'link': '/galleries/{gallery}/'.format(gallery=gallery_uri),
+            'thumbnail': '/img/{gallery}/thumbnail.jpg?h=280'.format(gallery=gallery_uri)
         })
     return render_template('galleries.html', galleries=galleries)
 
-@app.route('/galleries/<path:gallery_name>')
-def gallery(gallery_name):
-    gallery_path = p.join('galleries', gallery_name)
-    if not p.exists(gallery_path): abort(404)
-    gallery = {
-        'name': gallery_name,
-        'photos': []
-    }
-    photos_list = listdir(gallery_path)
-    for photo in photos_list:
-        photo_path = p.join(gallery_path, photo)
-        if not p.isfile(photo_path): continue
-        gallery['photos'].append({
-            'title': photo,
-            'src': '/img/' + photo_path + '?w=280'
-        })
-    return render_template('gallery.html', gallery=gallery)
+@app.route('/galleries/<path:gallery_uri>')
+def gallery(gallery_uri):
+    for gallery_name in listdir('galleries'):
+        if gallery_uri == getUri(gallery_name):
+            gallery_path = p.join('galleries', gallery_name)
+            if not p.exists(gallery_path): abort(404)
+            gallery = {
+                'name': gallery_name,
+                'photos': []
+            }
+            photos_list = listdir(gallery_path)
+            for photo in photos_list:
+                photo_path = p.join(gallery_path, photo)
+                if not p.isfile(photo_path): continue
+                gallery['photos'].append({
+                    'title': photo,
+                    'src': '/img/' + photo_path + '?w=280'
+                })
+            return render_template('gallery.html', gallery=gallery)
+    abort(404)
 
 @app.route('/img/galleries/<path:path>')
 def images(path):
